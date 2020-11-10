d4c9568ed57532b5b80c79b8265ab67f59370b7e
Danilo M <51140620+DaniloManchon@users.noreply.github.com>
2020-11-09 21:35:00 -0300

    add ConexaoDB

---
+++ src/main/java/conexao/ConexaoDB.java
@@ -0,0 +1,22 @@
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+    private String driver = "oracle.jdbc.driver.OracleDriver";
+    private String caminho = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+    private String login = "";
+    private String senha = "";
+    private Connection conexao;
+
+    public Connection conectar(){
+        try{
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(caminho, login, senha);
+        }catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conexao;
+    }
+}
