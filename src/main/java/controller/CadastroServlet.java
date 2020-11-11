58f62cbd2719616c8b07697f81f392969dcd779f
Danilo M <51140620+DaniloManchon@users.noreply.github.com>
2020-11-11 04:58:30 -0300

    add PistaServlet

--- src/main/java/controller/CadastroServlet.java
+++ src/main/java/controller/CadastroServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "CadastroServlet")
+@WebServlet(name = "CadastroServlet", value = "/cadastro")
 public class CadastroServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String button = request.getParameter("button");
