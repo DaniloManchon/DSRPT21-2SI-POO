71373acbfc1f6d6b20f9059fe53935c635cc2628
Danilo M <51140620+DaniloManchon@users.noreply.github.com>
2020-11-10 22:39:59 -0300

    login/cadastro criados

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,5 +1,47 @@
 package model;
 
 public class Usuario {
+    int id;
+    String name;
+    String password;
+    String userType;
 
+    public Usuario(int id, String name, String password, String userType) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
 }
